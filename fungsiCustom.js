84f351c73c875e6a41736e3ecf7da96332ba164b
Aziz Ahmadi <azizahmadi89@gmail.com>
2022-06-12 21:01:14 +0700

    Update fungsiCustom.js
    Membuat fungsi bacaData dengan parameter fnCallback

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+var fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,53 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const getMessage = (data) => {
+  const jsonData = JSON.parse(data);
+  if (jsonData.message !== undefined) {
+    const secondWord = jsonData.message.split(" ");
+    return secondWord[1];
+  } else if (jsonData[0].message !== undefined) {
+    const secondWord = jsonData[0].message.split(" ");
+    return secondWord[1];
+  } else if (jsonData[0].data.message !== undefined) {
+    const secondWord = jsonData[0].data.message.split(" ");
+    return secondWord[1];
+  } else {
+    return null;
+  }
+};
+
+let resultMessages = [];
+const bacaData = (fnCallback) => {
+  fs.readFile(file1, (err, data) => {
+    if (err) {
+      fnCallback(err);
+      return;
+    }
+    const newMessage = getMessage(data);
+    resultMessages.push(newMessage);
+    fs.readFile(file2, (err, data) => {
+      if (err) {
+        fnCallback(err);
+        return;
+      }
+      const newMessage = getMessage(data);
+      resultMessages.push(newMessage);
+      fs.readFile(file3, (err, data) => {
+        if (err) {
+          fnCallback(err);
+          return;
+        }
+        const newMessage = getMessage(data);
+        resultMessages.push(newMessage);
+        fnCallback(
+          null,
+          resultMessages
+        );
+      });
+    });
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
